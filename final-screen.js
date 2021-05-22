2b7a923efbf51b38d1658a8b28a160d15264e52e
Subashsunar <subashsunar00@gmail.com>
2021-05-22 04:18:52 -0600

    Minor changes

---
+++ final-screen.js
@@ -0,0 +1,25 @@
+const username = document.getElementById('username');
+const saveScoreBtn = document.getElementById('saveScoreBtn');
+const finalScore = document.getElementById('finalScore');
+const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem('highScores')) || [];
+
+const MAX_HIGH_SCORES = 5;
+
+finalScore.innerText = mostRecentScore;
+
+username.addEventListener('keyup', () => {
+    saveScoreBtn.disabled = !username.value;
+});
+
+saveHighScore = (e) => {
+    console.log("Clicked")
+    e.preventDefault();
+
+    const score = {
+        score: mostRecentScore,
+        name: username.value,
+    };
+    highScores.push(score);
+};
